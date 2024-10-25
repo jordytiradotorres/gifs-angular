f014638f4e7eb1c5d196aaa4cb68819c2ee5fa3a
Jordy Tirado Torres <jordytiradotorres20@gmail.com>
2024-10-24 22:12:36 -0500

    feat: validaciones gifService

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,20 +1,30 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GifsService {
-  constructor() { }
+  constructor() {}
 
   // es privado para que en otro lugar no haga las modificaciones de este servicio
-  private _tagHistory: string[] = []
+  private _tagHistory: string[] = [];
 
   get tagHistory() {
-    return [...this._tagHistory]
+    return [...this._tagHistory];
+  }
+
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+
+    if (this._tagHistory.includes(tag)) {
+      this._tagHistory = this._tagHistory.filter((oldTag) => oldTag !== tag);
+    }
+    this._tagHistory.unshift(tag);
+    this._tagHistory = this._tagHistory.splice(0, 10);
   }
 
   public searchTag(tag: string): void {
-    this._tagHistory.unshift(tag)
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
   }
-
 }
